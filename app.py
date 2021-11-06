f264440123b27c8a6b43d9e5146dc86757e7e304
Danieln7760 <nkemdirimdaniel@cmail.carleton.ca>
2021-11-06 00:44:41 -0400

    modified app.py

--- app.py
+++ app.py
@@ -5,4 +5,7 @@ def home():
     return 'Home Page'
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route('/interfaces')
+def interfaces():
+    return 'interfaces reached'
 
